dd5fe55ef2971bf25e8bcefba89e7be290e10cc9
Tejashwini2050 <107855898+Tejashwini2050@users.noreply.github.com>
2024-07-15 23:35:33 +0530

    Update StartApplication.java

--- src/main/java/com/abhishek/StartApplication.java
+++ src/main/java/com/abhishek/StartApplication.java
@@ -1,6 +1,7 @@
 package com.abhishek;
 //file with main class
 //new comment
+//new comment github
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
