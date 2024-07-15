9292fb690bebda50b3f6126bf77566e2858d3a4f
Tejashwini2050 <tejashwini.233@gmail.come>
2024-07-15 23:32:53 +0530

    new comment

--- src/main/java/com/abhishek/StartApplication.java
+++ src/main/java/com/abhishek/StartApplication.java
@@ -1,5 +1,6 @@
 package com.abhishek;
 //file with main class
+//new comment
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
